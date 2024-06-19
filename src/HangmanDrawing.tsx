36b87c0a941ddddcc4cf98e80a573f930a041c2c
Abhi <meabhishek1512@gmail.com>
2024-06-19 23:47:36 +0530

    hanging frame setup

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,5 +1,41 @@
 const HangmanDrawing = () => {
-    return <div>HangmanDrawing</div>
+    return (
+        <div
+            style={{
+                position: "relative",
+            }}
+        >
+            <div
+                style={{
+                    height: "50px",
+                    width: "10px",
+                    background: "black",
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                }}
+            />
+            <div
+                style={{
+                    height: "10px",
+                    width: "200px",
+                    background: "black",
+                    marginLeft: "120px",
+                }}
+            />
+            <div
+                style={{
+                    height: "400px",
+                    width: "10px",
+                    background: "black",
+                    marginLeft: "120px",
+                }}
+            />
+            <div
+                style={{ height: "10px", width: "250px", background: "black" }}
+            />
+        </div>
+    )
 }
 
 export default HangmanDrawing
