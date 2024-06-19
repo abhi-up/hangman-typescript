37779814d79b158203ced0b18d259a88d5b399e7
Abhi <meabhishek1512@gmail.com>
2024-06-19 23:38:59 +0530

    project structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import HangmanDrawing from "./HangmanDrawing"
+import HangmanWord from "./HangmanWord"
+import Keyboard from "./Keyboard"
 
 const App = () => {
     const [wordToGuess, setWordToGuess] = useState(() => {
@@ -9,7 +12,31 @@ const App = () => {
     const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
     console.log(wordToGuess)
-    return <div>App</div>
+    return (
+        <div
+            style={{
+                maxWidth: "800px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                margin: "0 auto",
+                alignItems: "center",
+            }}
+        >
+            <div
+                style={{
+                    fontSize: "2rem",
+                    textAlign: "center",
+                }}
+            >
+                Loss Win
+            </div>
+
+            <HangmanDrawing />
+            <HangmanWord />
+            <Keyboard />
+        </div>
+    )
 }
 
 export default App
